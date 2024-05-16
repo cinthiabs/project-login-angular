c9a086207021316effdeeccf2e143ca169bfaaf3
cinthiabs <cinthiabarbosa8d@outlook.com>
2024-05-16 08:08:11 -0300

    test login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,9 @@
-describe('template spec', () => {
+describe('Login', () => {
+
+  beforeEach(()=> {
+    cy.visit('/')
+  })
+  
   it('Valite element on Login', () => {
     cy.visit('/')
 
@@ -25,7 +30,7 @@ describe('template spec', () => {
 
 
   })
-  it('Login - Correct', () => {
+  it('Login - Correct with email  and passwprd incorret', () => {
     cy.visit('/')
 
     cy.get('[formcontrolname="email"] > .input-wrapper > .input-content > input')
@@ -35,7 +40,51 @@ describe('template spec', () => {
       .type('teste@teste!')
     
     cy.get('.btn-primary').should('not.be.disabled')
+    cy.get('.btn-primary').click()
+    cy.get('.ng-trigger').should('contain','Falha no login. Por favor, verifique seu email e senha.')
     
 
   })
+  
+  it('Login - Correct', () => {
+    cy.visit('/')
+
+    cy.get('[formcontrolname="email"] > .input-wrapper > .input-content > input')
+      .type('maria@gmail.com')
+
+    cy.get('[formcontrolname="password"] > .input-wrapper > .input-content > input')
+      .type('teste123')
+    
+    cy.get('.btn-primary').should('not.be.disabled')
+    cy.get('.btn-primary').click()
+    cy.get('.ng-trigger').should('contain','Login feito com sucesso!')
+  })
+})
+
+describe('Signup', () => {
+  it.only('Valite element on Signup', () => {
+
+      cy.get('.btn-secondary').click()
+     
+      cy.get('h2').contains('Sign in and start today!')
+      cy.get('[formcontrolname="name"] > .input-wrapper > .input-content > input')
+      .should('have.attr','placeholder','Cinthia')
+
+      cy.get('[formcontrolname="email"] > .input-wrapper > .input-content > input')
+        .should('have.attr','placeholder','cinthia@gmail.com')
+  
+      cy.get('[formcontrolname="password"] > .input-wrapper > .input-content > input')
+        .should('have.attr','placeholder','Password')
+
+      cy.get('[formcontrolname="passwordConfirm"] > .input-wrapper > .input-content > input')
+        .should('have.attr','placeholder','Password')
+      
+      cy.get('.btn-primary').should('have.attr', 'disabled')
+      cy.get('.btn-primary').contains('Signup Now')
+      cy.get('.btn-secondary').contains('Login Now')
+  
+  
+    })
+
+
 })
